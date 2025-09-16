9bf221239636bc6e7030dd69fd1e9338b061a774
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-09-16 10:14:32 +0200

    Initial commit + application.properties

---
+++ src/main/java/se/lexicon/g56lecturespringbootjpa/G56LectureSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g56lecturespringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G56LectureSpringbootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G56LectureSpringbootJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g56lecturespringbootjpa/G56LectureSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g56lecturespringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G56LectureSpringbootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
